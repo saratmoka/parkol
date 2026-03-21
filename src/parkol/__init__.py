ed6f38dcd4a32741215bf88ffd6f98567c769982
Sarat Moka <s.moka@unsw.edu.au>
2026-03-22 02:07:20 +1100

    v0.1.3: CFTP without boundary conditioning, improved solver selection

--- src/parkol/__init__.py
+++ src/parkol/__init__.py
@@ -7,5 +7,5 @@ Uses gamma-soft colouring and partial rejection sampling (PRS).
 from .sample import sample_coloring
 from .utils import verify_coloring
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = ["sample_coloring", "verify_coloring"]
