34f43c6726210894706f4fb3914c0b3e398b093d
Sarat Moka <s.moka@unsw.edu.au>
2026-04-05 23:44:48 +1000

    Bump to v0.2.1

--- src/parkol/__init__.py
+++ src/parkol/__init__.py
@@ -7,5 +7,5 @@ Uses gamma-soft colouring and partial rejection sampling (PRS).
 from .sample import sample_coloring
 from .utils import verify_coloring
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = ["sample_coloring", "verify_coloring"]
